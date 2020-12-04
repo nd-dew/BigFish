333e13902f92474fac5844c8553ec635362ba14b
AndyPietr <siakax@gmail.com>
2020-12-04 20:29:44 +0100

    Added main window

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import ctypes
-MessageBox = ctypes.windll.user32.MessageBoxW
-MessageBox(None, 'Big fish game', 'Window title', 0)
+import pygame
+import settings
+import sys
+from pygame.locals import *
 
-print("Hola, soy pez grande")
-print("Hello World and David")
-# ble ble comment 
+class BiggerFish:
+    def __init__(self):
+        pygame.init()
+        self.settings= settings.Settings()
+        self.screen= pygame.display.set_mode(self.settings.screen_size)
+        pygame.display.set_caption('Bigger Fish')
+
+    def run_game(self):
+        self.screen.fill(self.settings.bg_color)
+        pygame.display.flip()
+        while True: # main game loop
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+
+game= BiggerFish()
+game.run_game()
+
+
+# while True: # main game loop
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+#     pygame.display.update()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.screen_width=600 
+        self.screen_height=600
+        self.screen_size=(self.screen_width, self.screen_height)
+        self.bg_color= (233,233,233)
