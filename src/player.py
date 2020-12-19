f282fb6fa33af943556af39da74e8b67f835ce74
AndyPietr <siakax@gmail.com>
2020-12-19 20:16:04 +0100

    Added concept of enum to class player. Player can be in one of finite states

--- src/player.py
+++ src/player.py
@@ -1,7 +1,14 @@
+from enum import Enum
 import pygame
 
 class Player():
+
     def __init__(self, game):
+        class State(Enum):
+            stop = 1
+            left = 2
+            right = 3
+
         self.speed = 1
         self.size = [int(48*2), int(48*2)] # [width, height]
 
@@ -22,20 +29,19 @@ class Player():
         # Setting initial position
         self.rect.midbottom = self.screen_rect.midbottom # midbottom point of the screen is set to be equal with  midbnottom point of the player
 
-
-
         self.direction = "stop" # initial movement direction set to "stop"
+        self.currentState= State.stop
 
     def update(self):
         if self.direction == "right" and self.rect.right < self.screen_rect.right: # ...and player movement range restriction
             self.rect.x += self.speed
-            self.img = pygame.image.load("resources/images/sprite_sheets/tile038.png")  # tail left
+            self.img = self.sprites["tailLeft"]
         elif self.direction == "left" and self.rect.left > self.screen_rect.left:
             self.rect.x -= self.speed
-            self.img = pygame.image.load("resources/images/sprite_sheets/tile036.png")  # tail right
+            self.img = self.sprites["tailRight"]
         if self.direction == "stop": # to rethink
             self.rect.x += 0
-            self.img = pygame.image.load("resources/images/sprite_sheets/tile037.png")  # base image
+            self.img = self.sprites["steady"]
 
     def blit_player(self):
         self.screen.blit(self.img, self.rect)  # blit() method draws the image on top
@@ -43,3 +49,4 @@ class Player():
     def change_size(self):
         pass
         # changes the size of the picture
+
