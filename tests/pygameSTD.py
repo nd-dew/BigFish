42f1ff1dfe947fd089be8271c68defbbbcaa4e79
AndyPietr <siakax@gmail.com>
2020-12-19 17:05:44 +0100

    Added standard pygame template

---
+++ tests/pygameSTD.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([600, 600])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+        # if event.type == pygame.QUIT:
+        #     running = False  # Be interpreter friendly
+
+pygame.quit()
