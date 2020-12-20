5b7d7595d3bcd118ddf4b2bc7219e45e4bff762d
AndyPietr <siakax@gmail.com>
2020-12-20 08:30:29 +0100

    Added InnerClass Controls to hold keyboard states

--- src/biggerFish.py
+++ src/biggerFish.py
@@ -21,12 +21,15 @@ class BiggerFish:
         self.player = player.Player(self)  # player instance
         self.running = True
 
+        self.controls= self.Controls()
+
     def run_game(self):
         while self.running:  # Start of the game's main loop
             self.check_events()  # Event loop
             self.player.update()  # Checking the update method in PLAYER each loop.
             self.screen_update()  # Updating screen
-            self.clock.tick(60)
+            self.clock.tick(self.settings.FPS)
+            print(self.controls)
 
     def spawn(self):
         # create instance of enemy and append local list
@@ -35,23 +38,51 @@ class BiggerFish:
     def check_events(self):
         for event in pygame.event.get():
             
-            # KEYBOARD 
+            # QUIT GAME
             if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
                 self.running = False
+
+            # KEYBOARD INPUT
             elif event.type == pygame.KEYDOWN:  # Check for events when a keypress is done
                 if event.key == pygame.K_RIGHT:
-                    self.player.direction = "right"  # Also animate in here or in inside of move
+                    self.player.direction = "right"
+                    self.controls.right_down()
                 elif event.key == pygame.K_LEFT:
                     self.player.direction = "left"
+                    self.controls.left_down()
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.player.direction = "stop"
+                    self.controls.right_up()
                 elif event.key == pygame.K_LEFT:
                     self.player.direction = "stop"
+                    self.controls.left_up()
 
     def screen_update(self):
         self.screen.fill(self.settings.bg_color)  # Redrawing the background each pass
         self.player.blit_player()  # drawing our fish on top of our background
         # blit enemies in the screen (iterate over self.enemies )
         pygame.display.flip()  # TODO change to update
+
+    class Controls():
+        def __init__(self):
+            self.keyboard={'right':False,
+                           'left': False,}
+
+        def __str__(self):
+            return str(self.keyboard)
+
+        def right_down(self):
+            self.keyboard["right"]=True
+
+        def left_down(self):
+            self.keyboard["left"]=True
+
+        def right_up(self):
+            self.keyboard["right"]=False
+
+        def left_up(self):
+            self.keyboard["left"]=False
+
 
