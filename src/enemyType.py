4d2ff8ced15a474c4d848fb0feea1ee371d76afb
davidjuraj <ddjjszucs@gmail.com>
2021-01-24 19:13:55 +0100

    enemyType.py.py docstrings

--- src/enemyType.py
+++ src/enemyType.py
@@ -1,4 +1,26 @@
 class EnemyType():
+    """
+    This class regroups all the attributes that differ for each of the enemy type.
+
+    Attributes
+    ----------
+    speed : int
+        speed of the enemy
+    width : int
+        width of the enemy image
+    height : int
+        width of the enemu image
+    size : [int, int]
+        [width, height]
+    img_path_R : String
+        path to the image with right curved tail of the enemy
+    img_path_S : String
+        path to the image with steady tail of the enemy
+    img_path_L : String
+        path to the image with left curved tail of the enemy
+    hitbox_offset_and_size : [int, int, int, int]
+        specifying the offset from the image size where the collisions are occuring
+    """
     def __init__(self, speed, width, height,path_R, path_S, path_L, hitbox_offset_and_size=[0,0,10,10]):
         self.speed = speed
         self.width = width
