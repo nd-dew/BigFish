f9745f960055a578a0a88ae98e813a1efe0c358a
davidjuraj <ddjjszucs@gmail.com>
2021-01-24 05:16:17 +0100

    Formatting and documentation

--- src/enemyType.py
+++ src/enemyType.py
@@ -4,8 +4,9 @@ class EnemyType():
         self.width = width
         self.height = height
         self.size = [width, height]
+        # Paths to sprite images (image with right tail, tail steady, or left tail)
         self.img_path_R = path_R
         self.img_path_S = path_S
         self.img_path_L = path_L
-
-        self.hitbox_offset_and_size=hitbox_offset_and_size
+        # hitbox offset and size is used to specify the area that will be engaged in collisions
+        self.hitbox_offset_and_size = hitbox_offset_and_size
